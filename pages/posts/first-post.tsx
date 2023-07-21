a8da732e41c47fc62e79dade21ad785f20eddbb7
Matthew Creek <matthew.creek27@gmail.com>
2023-07-20 23:17:22 -0500

    troubleshooting profile photo issue

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -5,7 +5,7 @@ import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <Layout home>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
