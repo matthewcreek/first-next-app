230f2a3714dc098b3465ac0e85cfd24e82bff188
Matthew Creek <matthew.creek27@gmail.com>
2023-07-24 15:38:44 -0500

    fixed typo in imports that was breaking code

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-import { getSortedPostData } from '../lib/posts'
+import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
