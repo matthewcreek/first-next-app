12c31eda7d59bfb2cd7a2f864e5c2bb35742893b
Matthew Creek <matthew.creek27@gmail.com>
2023-07-24 16:53:34 -0500

    dynamic routes are working for blog posts based on id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,15 @@
 import Layout from "../../components/layout";
 
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
 
 export async function getStaticPaths() {
     const paths = getAllPostIds();
@@ -10,10 +19,14 @@ export async function getStaticPaths() {
     };
 }
 
-export default function Post() {
+export default function Post({ postData }) {
     return (
         <Layout>
-
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
         </Layout>
     )
 }
